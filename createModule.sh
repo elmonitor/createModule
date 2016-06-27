7bbf28d213e2067cf560a05b1f13be7060c0c116
elmonitor <info@elmonitor.net>
2016-06-27 15:41:14 +0000

    Create createModule.sh

---
+++ createModule.sh
@@ -0,0 +1,11 @@
+echo "Please write the module name:"
+read "my_module"
+mkdir $module_name
+cd $module_name
+mkdir models controllers views security data demo i18n -p static/description
+touch __init__.py __openerp__.py models/__init__.py controllers/__init__.py
+echo "# -*- coding: utf-8 -*-" >> __init__.py
+echo "from . import models, controllers" >> __init__.py
+echo "----------------------------------"
+echo "Module $module_name created"
+echo "----------------------------------"
