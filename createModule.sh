9d53622c6f48282566ad7b8eae15e3479802117d
elmonitor <info@elmonitor.net>
2016-10-19 14:07:33 -0500

    Update createModule.sh

--- createModule.sh
+++ createModule.sh
@@ -3,25 +3,12 @@ echo "Please write the module name:"
 read module_name
 mkdir $module_name
 cd $module_name
-mkdir controllers data demo doc i18n models report security -p static/description -p static/src test tests views wizard
-mkdir static/src/css static/src/img static/src/js static/src/less static/src/xml
+mkdir models views
 touch __init__.py __openerp__.py
-touch controllers/__init__.py
-touch data/${module_name}_data.xml
-touch demo/${module_name}_demo.xml
 touch models/__init__.py models/${module_name}.py
-touch report/__init__.py
-touch security/${module_name}_security.xml security/ir.model.access.csv
-touch static/src/css/${module_name}.css
-touch tests/__init__.py
 touch views/${module_name}.xml
-touch wizard/__init__.py
 echo "# -*- coding: utf-8 -*-" >> __init__.py
 echo "import models" >> __init__.py
-echo "import wizard" >> __init__.py
-echo "import report" >> __init__.py
-echo "import controllers" >> __init__.py
-echo "import tests" >> __init__.py
 echo "{" >> __openerp__.py
 echo "'name': $module_name" >> __openerp__.py
 echo "'category': ," >> __openerp__.py
