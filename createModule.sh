f4dcf455e19a00d5b8644a600ea823238ff2d63d
elmonitor <info@elmonitor.net>
2016-10-19 14:16:26 -0500

    Update createModule.sh

--- createModule.sh
+++ createModule.sh
@@ -10,16 +10,17 @@ touch views/${module_name}.xml
 echo "# -*- coding: utf-8 -*-" >> __init__.py
 echo "import models" >> __init__.py
 echo "{" >> __openerp__.py
-echo "'name': $module_name" >> __openerp__.py
-echo "'category': ," >> __openerp__.py
-echo "'sequence': ," >> __openerp__.py
-echo "'summary': ," >> __openerp__.py
-echo "'website': ," >> __openerp__.py
-echo "'version': ," >> __openerp__.py
-echo "'description': ," >> __openerp__.py
-echo "'demo': ," >> __openerp__.py
-echo "'qweb': ," >> __openerp__.py
-echo "'application': True," >> __openerp__.py
+echo "    'name': $module_name" >> __openerp__.py
+echo "    'category': ," >> __openerp__.py
+echo "    'sequence': ," >> __openerp__.py
+echo "    'summary': ," >> __openerp__.py
+echo "    'website': ," >> __openerp__.py
+echo "    'version': ," >> __openerp__.py
+echo "    'description': ," >> __openerp__.py
+echo "    'demo': ," >> __openerp__.py
+echo "    'qweb': ," >> __openerp__.py
+echo "    'application': True," >> __openerp__.py
+echo "}" >> __openerp__.py
 echo "# -*- coding: utf-8 -*-" >> models/__init__.py
 echo "# -*- coding: utf-8 -*-" >> models/${module_name}.py
 echo "----------------------------------"
