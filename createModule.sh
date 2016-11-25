9493a0dfc4723ebc9476f71d7bf0b797f54d0bec
elmonitor <info@elmonitor.net>
2016-11-25 16:46:53 -0500

    Update createModule.sh

--- createModule.sh
+++ createModule.sh
@@ -1,13 +1,90 @@
 echo "--------------------------------"
-echo "Please write the module name:"
-read module_name
-mkdir $module_name
+echo "Write the module name underscore:"
+read name
+sudo mkdir $name
+#access to module folder
+cd $name
+echo "Write the Odoo Version (8,9,10):"
+read odoo_v
+if [ "$odoo_v" == 8 ]; then
+  sudo touch __openerp__.py
+fi
+echo "Write the Module title description:"
+read title
+echo "Write the module version (1.0):"
+read version
+echo "Write the author (3nodus):"
+read author
+echo "Write the module category:"
+read category
+echo "Write the module summary:"
+read summary
+echo "Write the module description:"
+read description
+echo "Write the website:"
+read website
+echo "Write the dependences comma separated ('account','purchase'):"
+read depends
+
+echo "Create the folder 'models'?(y,n):"
+read answer
+if [ "$answer" == "y" -o "$answer" == "Y" ]; then
+  sudo mkdir $name/models
+fi
+echo "Create the folder views?(y,n):"
+read answer
+if [ "$answer" == "y" -o "$answer" == "Y" ]; then
+  sudo mkdir views
+fi
+
+echo "Create the folder  ?(y,n):"
+read answer
+if [ "$answer" == "y" -o "$answer" == "y" ]; then
+  sudo mkdir
+fi
+
+echo "Create the folder  ?(y,n):"
+read answer
+if [ "$answer" == "y" -o "$answer" == "y" ]; then
+  sudo mkdir
+fi
+
+echo "Create the folder  ?(y,n):"
+read answer
+if [ "$answer" == "y" -o "$answer" == "y" ]; then
+  sudo mkdir
+fi
+
+
+
+
+echo "Create the folder  ?(y,n):"
+read answer
+
+echo "Create the folder  ?(y,n):"
+read answer
+
+echo "Create the folder  ?(y,n):"
+read answer
+
+
+
+
+sudo mkdir $module_name
 cd $module_name
-mkdir models views
-touch __init__.py __openerp__.py
-touch models/__init__.py models/${module_name}.py
-touch views/${module_name}.xml
+sudo touch __init__.py
 echo "# -*- coding: utf-8 -*-" >> __init__.py
+echo "# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)." >> __init__.py
+echo "import models" >> __init__.py
+
+sudo touch __openerp__.py
+
+
+sudo mkdir models views
+
+sudo touch models/__init__.py models/${module_name}.py
+sudo touch views/${module_name}.xml
+
 echo "import models" >> __init__.py
 echo "{" >> __openerp__.py
 echo "    'name': $module_name" >> __openerp__.py
